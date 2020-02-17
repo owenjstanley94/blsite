4bec25c5afdb25e42637a192df44dfb02d80b966
owenjstanley94 <owenjstanley94@gmail.co.uk>
2020-02-17 14:42:46 +0000

    fixed gmaps intergration

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function initMap() {
-    let center = {lat: 41.8781, lng: -87.6298};
+    let center = {lat: 53.763757, lng: -2.695436};
     let map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
+      zoom: 9,
       center: center
     });
     let marker = new google.maps.Marker({
@@ -11,26 +11,28 @@ function initMap() {
   }
 
 
-//   let locations = [
-//     ['Los Angeles', 34.052235, -118.243683],
-//     ['Santa Monica', 34.024212, -118.496475],
-//     ['Redondo Beach', 33.849182, -118.388405],
-//     ['Newport Beach', 33.628342, -117.927933],
-//     ['Long Beach', 33.770050, -118.193739]
-//   ];
+let locations = [
+    ['Los Angeles', 34.052235, -118.243683],
+    ['Santa Monica', 34.024212, -118.496475],
+    ['Redondo Beach', 33.849182, -118.388405],
+    ['Newport Beach', 33.628342, -117.927933],
+    ['Long Beach', 33.770050, -118.193739]
+  ];
 
-//   let infowindow =  new google.maps.InfoWindow({});
-// let marker, count;
-// for (count = 0; count < locations.length; count++) {
-//     marker = new google.maps.Marker({
-//       position: new google.maps.LatLng(locations[count][1], locations[count][2]),
-//       map: map,
-//       title: locations[count][0]
-//     });
-// google.maps.event.addListener(marker, 'click', (function (marker, count) {
-//       return function () {
-//         infowindow.setContent(locations[count][0]);
-//         infowindow.open(map, marker);
-//       }
-//     })(marker, count));
-//   } 
+let infowindow =  new google.maps.InfoWindow({});
+
+let marker, count;
+
+for (count = 0; count < locations.length; count++) {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[count][1], locations[count][2]),
+      map: map,
+      title: locations[count][0]
+    });
+google.maps.event.addListener(marker, 'click', (function (marker, count) {
+      return function () {
+        infowindow.setContent(locations[count][0]);
+        infowindow.open(map, marker);
+      }
+    })(marker, count));
+  } 
